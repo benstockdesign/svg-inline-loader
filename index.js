cd9d70f8798950510f5a4a8f664ddb68e066fe68
Ben Stock <benstockdesign@gmail.com>
2020-06-23 16:28:29 -0500

    Update index.js
    - Convert `var` keywords to `let` and `const` keywords.
    - Remove trailing semicolons.
    - Break down `getExtractedSVG` function into smaller parts to see how things work.

--- index.js
+++ index.js
@@ -1,72 +1,80 @@
-var simpleHTMLTokenizer = require('simple-html-tokenizer');
-var tokenize = simpleHTMLTokenizer.tokenize;
-var generate = simpleHTMLTokenizer.generate;
-var loaderUtils = require('loader-utils');
-var assign = require('object-assign');
+const simpleHTMLTokenizer = require('simple-html-tokenizer')
+const tokenize = simpleHTMLTokenizer.tokenize
+const generate = simpleHTMLTokenizer.generate
+const loaderUtils = require('loader-utils')
+const assign = require('object-assign')
 
-var conditions = require('./lib/conditions');
-var transformer = require('./lib/transformer');
+const conditions = require('./lib/conditions')
+const transformer = require('./lib/transformer')
 
-// TODO: find better parser/tokenizer
-var regexSequences = [
-    // Remove XML stuffs and comments
-    [/<\?xml[\s\S]*?>/gi, ""],
-    [/<!doctype[\s\S]*?>/gi, ""],
-    [/<!--.*-->/gi, ""],
+const replacements = [
+    [/<\?xml[\s\S]*?>/gi, ""],                     // Remove XML element
+    [/<!doctype[\s\S]*?>/gi, ""],                  // Remove doctype
+    [/<!--.*-->/gi, ""],                           // Remove comments
+    [/\<([A-Za-z]+)([^\>]*)\/\>/g, "<$1$2></$1>"], // Convert self-closing XML SVG nodes to explicitly-closed HTML5 SVG nodes
+    [/\s+/g, " "],                                 // Replace runs of whitespace with a single space
+    [/\> \</g, "><"]                               // Remove whitespace between tags
+]
 
-    // SVG XML -> HTML5
-    [/\<([A-Za-z]+)([^\>]*)\/\>/g, "<$1$2></$1>"], // convert self-closing XML SVG nodes to explicitly closed HTML5 SVG nodes
-    [/\s+/g, " "],                                 // replace whitespace sequences with a single space
-    [/\> \</g, "><"]                               // remove whitespace between tags
-];
+const defaultHash = "__[hash:base64:7]__"
 
-function getExtractedSVG(svgStr, options) {
-    var config;
-    // interpolate hashes in classPrefix
-    if(!!options) {
-        config = assign({}, options);
+function sanitizeSVG(svg) {
+    let result = svg
+    for (const [pattern, replacement] of replacements) {
+        result = result.replace(pattern, replacement)
+    }
+    return result.trim()
+}
 
-        if (!!config.classPrefix) {
-            const name = config.classPrefix === true ? '__[hash:base64:7]__' : config.classPrefix;
-            config.classPrefix = loaderUtils.interpolateName({}, name, { content: svgStr });
-        }
-
-        if (!!config.idPrefix) {
-            const id_name = config.idPrefix === true ? '__[hash:base64:7]__' : config.idPrefix;
-            config.idPrefix = loaderUtils.interpolateName({}, id_name, { content: svgStr });
+function resolveConfig(options, content) {
+    if (!!options) {
+        return undefined
+    }
+    const config = assign({}, options)
+    let name = undefined
+    // Interpolate hashes for `classPrefix` and `idPrefix` options
+    for (const key of ["classPrefix", "idPrefix"]) {
+        name = config[key]
+        if (typeof name === "string") {
+            if (name.length === 0) {
+                name = defaultHash
+            }
+            config[key] = loaderUtils.interpolateName({}, name, {content})
         }
     }
+    return config
+}
 
-    // Clean-up XML crusts like comments and doctype, etc.
-    var tokens;
-    var cleanedUp = regexSequences.reduce(function (prev, regexSequence) {
-        return ''.replace.apply(prev, regexSequence);
-    }, svgStr).trim();
-
-    // Tokenize and filter attributes using `simpleHTMLTokenizer.tokenize(source)`.
+function getExtractedSVG(svg, options) {
+    // Get resolved configuration options.
+    const config = resolveConfig(options, svg)
+    // Clean up input SVG string.
+    const result = sanitizeSVG(svg)
+    // Tokenize and filter attributes using `simpleHTMLTokenizer.tokenize(svg)`.
+    let tokens = undefined
     try {
-        tokens = tokenize(cleanedUp);
-    } catch (e) {
-        // If tokenization has failed, return earlier with cleaned-up string
-        console.warn('svg-inline-loader: Tokenization has failed, please check SVG is correct.');
-        return cleanedUp;
+        tokens = tokenize(result)
+    } catch (_) {
+        // If tokenization has failed, return sanitized string.
+        console.warn("[svg-inline-loader] Invalid SVG: tokenization failed")
+        return result
     }
-
     // If the token is <svg> start-tag, then remove width and height attributes.
-    return generate(transformer.runTransform(tokens, config));
+    return generate(transformer.runTransform(tokens, config))
 }
 
 function SVGInlineLoader(content) {
-    this.cacheable && this.cacheable();
-    this.value = content;
-    // Configuration
-    var options = loaderUtils.getOptions(this);
-
-    return "module.exports = " + JSON.stringify(getExtractedSVG(content, options));
+    if (this.cacheable) {
+        this.cacheable()
+    }
+    this.value = content
+    const options = loaderUtils.getOptions(this)
+    const result = JSON.stringify(getExtractedSVG(content, options))
+    return `module.exports = ${result}`
 }
 
-SVGInlineLoader.getExtractedSVG = getExtractedSVG;
-SVGInlineLoader.conditions = conditions;
-SVGInlineLoader.regexSequences = regexSequences;
+SVGInlineLoader.getExtractedSVG = getExtractedSVG
+SVGInlineLoader.conditions = conditions
+SVGInlineLoader.regexSequences = regexSequences
 
-module.exports = SVGInlineLoader;
+module.exports = SVGInlineLoader
