87d409f76bdc7d2244cf2976951cea107120bc50
Ben Stock <benstockdesign@gmail.com>
2020-06-23 14:30:37 -0500

    Simplify configuration options
    - Remove the `removeTags` option
    - Rename a few config keys
    - Use `null` instead of `false` to indicate unspecified option values

--- config.js
+++ config.js
@@ -1,15 +1,13 @@
+// The original implementation (https://github.com/webpack-contrib/svg-inline-loader) 
+// removes the `title`, `desc`, `defs`, and `style` tags when the `removeTags` option 
+// is set to `true`. This version, on the other hand, drops the `removeTags` option 
+// altogether, and defers to the tags listed explicitly for removal.
 module.exports = {
-    removeSVGTagAttrs: true,
-    removeTags: false,
-    removingTags: [
-        'title',
-        'desc',
-        'defs',
-        'style'
-    ],
-    removingTagAttrs: [],
-    classPrefix: false,
-    idPrefix: false,
-    warnTags: [],
-    warnTagAttrs: []
+    classPrefix: null,
+    idPrefix: null,
+    removedTags: [],
+    removedAttributes: [],
+    removeRootSVGAttributes: true,
+    warningTags: [],
+    warningAttributes: []
 };
